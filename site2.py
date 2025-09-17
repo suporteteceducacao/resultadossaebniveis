1637bb5647b0ab8ef9ebefbf01726578254c39a5
SuporteTecEducação <127565732+suporteteceducacao@users.noreply.github.com>
2025-09-17 13:29:33 -0300

    Update site2.py

--- site2.py
+++ site2.py
@@ -13,7 +13,7 @@ try:
 except st.errors.StreamlitAPIException:
     pass
 
-caminho_planilha = "xls/Pasta_1.xlsx"
+caminho_planilha = "xls/Pasta_2.xlsx"
 caminho_logo = "img/logo_2021.png"
 
 @st.cache_data
@@ -251,3 +251,4 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+
