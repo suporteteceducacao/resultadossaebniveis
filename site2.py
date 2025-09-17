ffc5de9480f89d619d09e184a59bc6fce6a50540
SuporteTecEducação <127565732+suporteteceducacao@users.noreply.github.com>
2025-09-17 13:20:56 -0300

    Update site2.py

--- site2.py
+++ site2.py
@@ -238,10 +238,11 @@ st.markdown(
 st.markdown(
     """
     <footer style="text-align:center; color:#888; margin-top:50px; padding:15px 0; font-size:14px; font-family: Arial, sans-serif; border-top: 1px solid #ddd;">
-    © 2025 Desenvolvido por sua equipe de análise
+    © 2025 Resultados SAEB por nível. Setor de Processamento e Monitoramento de Resultados - SPMR-DAM/SME Maracanaú. <br>
     </footer>
     """,
     unsafe_allow_html=True,
 )
+
 
 
